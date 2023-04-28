64cb000281bde0f9b2914b0073edac7f93e108c4
Huw Williams <huwmorgan91@gmail.com>
2023-04-28 15:43:57 +0100

    font awesome icons added to project cards

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import styles from './ProjectCard.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faRocket } from '@fortawesome/free-solid-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 const ProjectCard = () => {
   return (
     <div className={styles.projectCard}>
       <h1 className={styles.projectName}>Project Name</h1>
       <img src="./images/huw-godzilla.jpg" alt="Huw with Godzilla" />
+      <span className={styles.linkIcons}>
+        <FontAwesomeIcon icon={faGithub} />
+        <FontAwesomeIcon icon={faRocket} />
+      </span>
     </div>
   )
 }
