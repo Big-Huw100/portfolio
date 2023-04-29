0734c280c76f049233478f34f88f4e68ab769e2a
Huw Williams <huwmorgan91@gmail.com>
2023-04-29 16:48:35 +0100

    Created Home and Contact Page containers.

---
+++ src/containers/Home/Home.jsx
@@ -0,0 +1,21 @@
+import styles from './App.module.scss';
+import Header from './components/Header';
+import Intro from './components/Intro';
+import ProjectGrid from './components/ProjectGrid/ProjectGrid';
+import Footer from './components/Footer';
+
+const Home = () => {
+
+  return (
+    <>
+    <main className={styles.App}>
+      <Header />
+      <Intro />
+      <ProjectGrid />
+      <Footer />
+    </main>
+    </>
+  );
+}
+
+export default Home;
