18fd6b772101ae6c75906fd274470a8811b217f3
Huw Williams <huwmorgan91@gmail.com>
2023-05-02 14:17:31 +0100

    Finished some basic styling on the Contact Form

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import styles from './ContactForm.module.scss';
 
 const ContactForm = () => {
     const [formData, setFormData] = useState({
@@ -17,17 +18,17 @@ const ContactForm = () => {
       };
 
   return (
-     <form onSubmit={handleSubmit}>
-      <label htmlFor="name">Name:</label>
+     <form className={styles.ContactForm} onSubmit={handleSubmit}>
+      <label htmlFor="name">Name</label>
       <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} />
 
-      <label htmlFor="email">Email:</label>
+      <label htmlFor="email">Email</label>
       <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} />
 
-      <label htmlFor="message">Message:</label>
+      <label htmlFor="message">Message</label>
       <textarea id="message" name="message" value={formData.message} onChange={handleChange} />
 
-      <button type="submit">Submit</button>
+      <button type="submit">Send</button>
     </form>
   )
 }
--- src/containers/ContactPage/ContactPage.jsx
+++ src/containers/ContactPage/ContactPage.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../../components/Header';
 import ContactForm from '../../components/ContactForm/ContactForm';
+import Footer from '../../components/Footer'
 
 function ContactPage() {
   return (
     <>
     <Header />
     <ContactForm />
+    <Footer />
     </>
   )
 }
