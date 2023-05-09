2480f2b457d59600d16465f48e97d10899e82400
Huw Williams <huwmorgan91@gmail.com>
2023-05-09 16:42:51 +0100

    Contact form links to gmail via EmailJS

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,22 +1,32 @@
-import React from 'react';
-import styles from './ContactForm.module.scss';
+import React, { useRef } from 'react';
+import styles from './ContactForm.module.scss'
+import emailjs from 'emailjs-com';
 
 const ContactForm = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('contact_service', 'contact_form', form.current, 'HlAvtwUIbBjoVREC2')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
 
   return (
-     <form className={styles.ContactForm}>
-      <label htmlFor="name">Name</label>
-      <input type="text" id="name" name="name" />
-
-      <label htmlFor="email">Email</label>
-      <input type="email" id="email" name="email" />
-
-      <label htmlFor="message">Message</label>
-      <textarea id="message" name="message" />
-
-      <button type="submit">Send</button>
+    <form className={styles.ContactForm} ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input className={styles.button} type="submit" value="Send" />
     </form>
-  )
-}
+  );
+};
 
 export default ContactForm
