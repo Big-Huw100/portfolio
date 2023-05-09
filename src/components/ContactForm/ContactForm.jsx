f5fce4c68e098d8a69af468ecbc0721c4abbfde1
Huw Williams <huwmorgan91@gmail.com>
2023-05-09 13:12:03 +0100

    Reverted back to pre firebase

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,45 +1,20 @@
-import React, { useState } from 'react';
-import { db } from '../../firebaseConfig';
-import { addDoc, collection } from 'firebase/firestore'
+import React from 'react';
 import styles from './ContactForm.module.scss';
 
 const ContactForm = () => {
-    const [name,setName] = useState();
-    const [email,setEmail] = useState();
-    const [message,setMessage] = useState();
-
-    const userCollectionRef = collection(db,"contactdata")
-
-    const handleSubmit = () => {
-      addDoc(userCollectionRef,{
-        name: name,
-        email: email,
-        message: message
-      }).then(() => {
-        if(!alert("Form Submitted Successfully!!!")) document.location = 'https://www.google.co.uk/'
-      }).catch((error) => {
-        alert(error.message)
-      })
-    }
 
   return (
      <form className={styles.ContactForm}>
       <label htmlFor="name">Name</label>
-      <input type="text" id="name" name="name" onChange={(event) => {
-        setName(event.target.value)
-      }} />
+      <input type="text" id="name" name="name" />
 
       <label htmlFor="email">Email</label>
-      <input type="email" id="email" name="email" onChange={(event) => {
-        setEmail(event.target.value)
-      }} />
+      <input type="email" id="email" name="email" />
 
       <label htmlFor="message">Message</label>
-      <textarea id="message" name="message" onChange={(event) => {
-        setMessage(event.target.value)
-      }} />
+      <textarea id="message" name="message" />
 
-      <button onClick={handleSubmit} type="submit">Send</button>
+      <button type="submit">Send</button>
     </form>
   )
 }
