cc6e1f1cef576a881cf07123755e1c75d445432d
Huw Williams <huwmorgan91@gmail.com>
2023-05-18 14:26:53 +0100

    Put the App.jsx back to how it was

--- src/App.jsx
+++ src/App.jsx
@@ -5,35 +5,34 @@ import ContactPage from './containers/ContactPage';
 
 const App = () => {
 
-  // const router = createBrowserRouter(
-  //   createRoutesFromElements(
-  //     <Route path="/" element={<Root />}>
-  //       <Route index element={<Home />} />
-  //       <Route path="/contact" element={<ContactPage />} />
-  //     </Route>
-  //   )
-  // )
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Root />}>
+        <Route index element={<Home />} />
+        <Route path="/contact" element={<ContactPage />} />
+      </Route>
+    )
+  )
 
   // comment
 
   return (
     <>
     <main className={styles.App}>
-      {/* <RouterProvider router={router} /> */}
-      <h1>works</h1>
+      <RouterProvider router={router} />
     </main>
     </>
   );
 }
 
-// const Root = () => {
-//   return (
-//     <>
-//       <div>
-//         <Outlet />
-//       </div>
-//     </>
-//   )
-// }
+const Root = () => {
+  return (
+    <>
+      <div>
+        <Outlet />
+      </div>
+    </>
+  )
+}
 
 export default App;
