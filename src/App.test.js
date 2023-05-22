7c204f144ce904ff9182a7e3b2fae65c33e6b7dc
Huw Williams <huwmorgan91@gmail.com>
2023-05-22 16:09:50 +0100

    unit testing for App.jsx

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,29 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import App from './App';
+
+describe('App', () => {
+  it('renders the Home component for the index route', () => {
+    const { getByText } = render(
+      <MemoryRouter initialEntries={['/']}>
+        <App />
+      </MemoryRouter>
+    );
+
+    expect(getByText('Home Component')).toBeInTheDocument();
+  });
+
+  it('renders the ContactPage component for the "/contact" route', () => {
+    const { getByText } = render(
+      <MemoryRouter initialEntries={['/contact']}>
+        <App />
+      </MemoryRouter>
+    );
+
+    expect(getByText('ContactPage Component')).toBeInTheDocument();
+  });
+});
