113e5b845a4ab5d6beb5ceef0ab37fca1afb7ed4
Huw Williams <huwmorgan91@gmail.com>
2023-06-20 15:58:26 +0100

    changed home link in header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ import thisisme from '../../images/huw-godzilla.jpg';
 const Header = () => {
   return (
     <section className={styles.header}>
-      <Link to="/portfolio"><img className={styles.img} src={thisisme} alt="Me with Godzilla" /></Link>
+      <Link to="/"><img className={styles.img} src={thisisme} alt="Me with Godzilla" /></Link>
       <section className={styles.title}>
         <h1 className={styles.name}>Huw Williams</h1>
         <h2 className={styles.job}>Web Developer</h2>
